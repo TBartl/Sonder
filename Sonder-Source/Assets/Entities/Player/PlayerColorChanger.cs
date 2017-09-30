4ae4cfa57123e3efa94deb522405bbe575ba5f42
TBartl <tbartl@umich.edu>
2017-09-30 16:21:21 -0400

    Player color change

---
+++ Sonder-Source/Assets/Entities/Player/PlayerColorChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct PlayerColorChange {
+    public Texture body;
+    public Texture hair;
+
+}
+
+public class PlayerColorChanger : MonoBehaviour {
+
+    public SkinnedMeshRenderer body;
+    public SkinnedMeshRenderer hair;
+
+    public List<PlayerColorChange> changes;
+
+    static int last = -1;
+
+	// Use this for initialization
+	void Start () {
+        int num = -1;
+        while (num == -1 || num == last) {
+            num = Random.Range(0, changes.Count);
+        }
+        last = num;
+        body.material.mainTexture = changes[num].body;
+        hair.material.mainTexture = changes[num].hair;
+    }
+}
